566e89c1c49a6314433add3ab37ceb1957573596
aman18yadav <amanyyadav691@gmail.com>
2021-09-08 19:10:29 +0530

    typing speed check

---
+++ main.js
@@ -0,0 +1,9 @@
+const setofwords=[
+"Hello!!.Welcome to my bootcamp",
+"My Name is AMAN YADAV","i am a student.",
+"if you want the sourse code you should go on my github accound Aman18yadav."
+];
+const msg= document.getElementById('msg');
+const typewords= document.getElementById('mywords');
+const btn= document.getElementById('btn');
+let startTime , endTime;
